7483b5cd44d66692b227d6f52f4482f4868863b4
humzarana <humza.abid@gmx.de>
2026-08-28 01:46:51 +0200

    Update config.js from content Sheet

--- config.js
+++ config.js
@@ -239,11 +239,9 @@ window.CONFIG = {
     }
   ],
   "openSeats": [
-    "churchBench:church bench:2",
-    "fold:folding:1",
-    "chaise:chaise:1",
-    "black:black chair:1",
-    "sofa:sofa:2",
-    "floor:floor:1"
+    "bench",
+    "black chair",
+    "sofa",
+    "chaise"
   ]
 };
